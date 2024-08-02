65d23ff83c8349a0fe61b59fbe6908926b22bc61
nbelkacem <nass.belkacem7@gmail.com>
2024-08-02 02:00:06 +0200

    ajout du Header et de ses composants enfants

---
+++ src/components/buttons/Accueil.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Accueil: React.FC = () => {
+    return (
+        <h1>Accueil</h1>
+    )
+}
+
+export default Accueil; 
---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Accueil from "../buttons/Accueil";
+import Services from "../buttons/Services";
+import Contact from "../buttons/Contact";
+
+interface HeaderProps {
+    name: string;
+}
+
+const Header : React.FC<HeaderProps> = ({ }) => {
+    return (
+        <div >
+        <Accueil/>
+        <Services/>
+        <Contact/>
+    </div>
+    )
+}
+
+export default Header;
