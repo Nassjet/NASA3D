2cb06615fc88cb3715bf9e0939d737234841fc06
nbelkacem <nass.belkacem7@gmail.com>
2024-08-07 20:58:44 +0200

    header terminé, début footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 
+import Footer from './components/ui/Footer';
 import Header from './components/ui/Header';
 
 function App() {
   return (
-
-      <Header name = "Bienvenue chez Nasa3D"/>
+    <>
+    <Header name = "Bienvenue chez Nasa3D"/>
+    <Footer name = "truc"/>
+    </>
   );
 }
 
--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,1 +1,26 @@
 import React from "react";
+import {
+    MDBFooter,
+    MDBContainer,
+    MDBCol,
+    MDBRow,
+    MDBIcon,
+    MDBBtn
+  } from 'mdb-react-ui-kit';
+
+interface FooterProps {
+    name: string;
+}
+
+const Footer : React.FC<FooterProps> = ({ }) => {
+    return (
+        <footer className="footer">
+            <div>
+
+            </div>
+        </footer>
+
+    )
+}
+
+export default Footer;
